f60d4d3a013b73b97448ff58444f0a40a0031da3
SergiioDev <sergi13456@hotmail.es>
2022-02-08 11:15:40 +0100

    create a validator class for updating the users

--- users/utils/validator.py
+++ users/utils/validator.py
@@ -43,3 +43,38 @@ class UserSchema(Schema):
                 raise ValidationError('This username is registered')
 
         return data
+
+
+class UserUpdateSchema(Schema):
+    email = fields.Email(required=True)
+    username = fields.Str(required=True)
+    first_name = fields.Str(required=True)
+
+    @post_load
+    def encrypt_password(self, data, **kwargs):
+        data["password"] = encrypt(data["password"])
+        return data
+
+    @post_load
+    def validate_email(self, data, **kwargs):
+        mongo = db.MongoDBConnection()
+        with mongo:
+            database = mongo.connection["myDB"]
+            collection = database["users"]
+
+            if collection.find_one({"email": data["email"]}) is not None:
+                raise ValidationError('This email is registered')
+
+        return data
+
+    @post_load
+    def validate_username(self, data, **kwargs):
+        mongo = db.MongoDBConnection()
+        with mongo:
+            database = mongo.connection["myDB"]
+            collection = database["users"]
+
+            if collection.find_one({"username": data["username"]}) is not None:
+                raise ValidationError('This username is registered')
+
+        return data
